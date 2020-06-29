c44fd4a2d9892d33caf9307f7adcc3e1be2a4b26
mayconbalves <mayconmustaine@hotmail.com>
2020-06-29 19:24:57 -0300

    feat(backend): create controller for method post and get discovery

---
+++ backend/src/controllers/discovery.controller.js
@@ -0,0 +1,19 @@
+const Discovery = require('../models/discovery.model')
+
+module.exports = {
+  async discovery_create(req, res) {
+    const discovery = await Discovery.create({
+      found: req.body.found,
+      description: req.body.description,
+      dateTime: req.body.dateTime
+    })
+
+    return res.json(discovery)
+  },
+
+  async discovery_list(req, res) {
+    const discovery = await Discovery.find({})
+
+    return res.json(discovery)
+  }
+}
