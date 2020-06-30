89d5c6f81d4a4a2b097349bf1f9c2947b5c38839
mayconbalves <mayconmustaine@hotmail.com>
2020-06-30 18:58:11 -0300

    feat(frontend): create simple reset of styled

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './Root'
 import store from './store/store.dev'
+import { createGlobalStyle } from 'styled-components'
 
-ReactDOM.render(<Root store={store} />, document.getElementById('root'))
+const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <Root store={store} />
+  </React.Fragment> , document.getElementById('root'))
 
 
