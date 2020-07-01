34b451a06c1608ee2135684edb0ef963e5536ec5
mayconbalves <mayconmustaine@hotmail.com>
2020-07-01 20:26:22 -0300

    feat(frontend): create anotations router

--- frontend/src/Root.js
+++ frontend/src/Root.js
@@ -3,12 +3,14 @@ import PropTypes from 'prop-types'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import HomeContainer from './containers/home/index'
+import AnotationContainer from './containers/anotations/index'
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <Router>
       <Switch>
         <Route exact path='/' component={HomeContainer} />
+        <Route exact path='/anotations' component={AnotationContainer} />
       </Switch>
     </Router>
   </Provider>
